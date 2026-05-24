d1996eb8e346888797534d6318999c75de989ee3
EliaSalerno <salel89.do@gmail.com>
2026-05-24 22:24:42 +0200

    Master: update repo add arduino

---
+++ arduino/sketch.ino
@@ -0,0 +1,42 @@
+#include <DHT.h>
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+#define PROXIMITY_PIN 3
+
+DHT dht(DHTPIN, DHTTYPE);
+
+struct SensorPacket {
+  float temperature;
+  float humidity;
+  bool classroomOccupied;
+  unsigned long timestamp;
+};
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+  pinMode(PROXIMITY_PIN, INPUT);
+}
+
+void loop() {
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+
+  if (isnan(temperature) || isnan(humidity)) {
+    delay(1000);
+    return;
+  }
+
+  bool classroomOccupied = digitalRead(PROXIMITY_PIN) == HIGH;
+
+  SensorPacket packet;
+  packet.temperature = temperature;
+  packet.humidity = humidity;
+  packet.classroomOccupied = classroomOccupied;
+  packet.timestamp = millis();
+
+  Serial.write((byte*)&packet, sizeof(packet));
+
+  delay(1000);
+}
